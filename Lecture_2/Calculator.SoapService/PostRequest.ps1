a7227a854b6034e521bd14d707ec02706f3b0fe6
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-04-09 12:44:06 +0200

    post request powershell script

---
+++ Lecture_2/Calculator.SoapService/PostRequest.ps1
@@ -0,0 +1,15 @@
+﻿$body = @"
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <Add xmlns="http://localhost:5255/soap">
+      <a>7</a>
+      <b>9</b>
+    </Add>
+  </soap:Body>
+</soap:Envelope>
+"@
+
+Invoke-WebRequest -Uri "http://localhost:5255/soap" `
+  -Method Post `
+  -ContentType "text/xml; charset=utf-8" `
+  -Body $body
